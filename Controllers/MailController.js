95c7451aff84550a22e6d4fe00f3f4fbec1ab1b6
Pankaj Pancholi <pancholipankaj3008@gmail.com>
2026-05-20 22:29:08 +0530

    2525

--- Controllers/MailController.js
+++ Controllers/MailController.js
@@ -69,7 +69,7 @@ async function sendBookingMail({ customer, bookingId, events, addons, estimate, 
     // });
    const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.BREVO_USER,
