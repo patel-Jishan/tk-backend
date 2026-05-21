8fd7cd85fdf0bfde8290fc5fb6367ab0024b72ea
Jishan Patel <jishanpatel.15072003@gmail.com>
2026-05-21 16:02:39 +0530

    Add contact routes to the application

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ const adminRoutes = require("./Routes/admin.routes");
 const bookingRoutes = require("./Routes/booking.routes");
 const photographerRoutes = require("./Routes/photographer.routes");
 const serviceRoutes = require("./Routes/service.routes");
+const contactRoutes = require("./Routes/contact.routes");
 
 // Routes use
 app.use("/api/admin", adminRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/photographers", photographerRoutes);
 app.use("/api/services", serviceRoutes);
+app.use("/api/contactUs", contactRoutes);
 
 // Test route
 app.get("/", (req, res) => {
