897febf9f4dbd22549c4a6e475640a5b849e13ea
Jishan Patel <jishanpatel.15072003@gmail.com>
2026-05-30 14:36:37 +0530

    Add CORS middleware for local development

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}))
 app.use(cookieParser());
 
 // Routes import
